9eefed78f87e93c6089da160b7796d6724a0a8ed
Edward Ovalle <edward.ovalle@cun.edu.co>
2026-07-04 23:23:11 -0500

    Verificar usuario que registra en git

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 def asegurar_chromium_playwright() -> tuple[bool, str]:
     """
-    En Streamlit Cloud, pip instala la librería Playwright,
+    En Streamlit Cloud, pip instala la librería Playwright,1
     pero no siempre descarga el navegador Chromium.
 
     Esta función descarga Chromium una sola vez por entorno.
